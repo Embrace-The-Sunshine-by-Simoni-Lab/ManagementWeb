7ac2d79518561f0cd6fea97be29dbef9fb48115b
wsheng24 <87457062+wsheng24@users.noreply.github.com>
2022-09-22 18:52:36 -0700

    Calendar works now, Use API as next step

--- src/app/views/dashboard/Analytics2.jsx
+++ src/app/views/dashboard/Analytics2.jsx
@@ -4,7 +4,8 @@ import ReactEcharts from 'echarts-for-react'
 // import { useTheme } from '@mui/system'
 import * as echarts from 'echarts';
 // import * as fs from 'fs';
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
+import axios from 'axios';
 
 const AnalyticsRoot = styled('div')(({ theme }) => ({
     margin: '30px',
@@ -13,39 +14,22 @@ const AnalyticsRoot = styled('div')(({ theme }) => ({
     },
 }))
 
-/*
-function getVirtulData(year) {
-    year = year || '2017';
-    var date = +echarts.number.parseDate(year + '-01-01');
-    var end = +echarts.number.parseDate(+year + 1 + '-01-01');
-    var dayTime = 3600 * 24 * 1000;
-    var data = [];
-    for (var time = date; time < end; time += dayTime) {
-        data.push([
-            echarts.format.formatTime('yyyy-MM-dd', time),
-            Math.floor(Math.random() * 10000)
-        ]);
-    }
-    console.log(data);
-    return data;
-}*/
-
 const Analytics2 = ({ height, color = [] }) => {
-    const [summarize_data, useSummarizeData] = useState({});
+    const [summarizeData, UseSummarizeData] = useState({ users: [] });
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios(
+                './generated.json',
+            );
 
-    function readActivityData(filename) {
-        fetch(filename)
-            .then((response) => {
-                response.json().then((data) => {
-                    // return useFileData(data);
-                    useSummarizeData(data)
-                }).catch((err) => {
-                    console.log(err);
-                })
-            });
-    }
+            UseSummarizeData(result.data);
+        };
 
-    function summarizeData(data) {
+        fetchData();
+    }, []);
+
+    function summarize(data) {
         let users = data.users;
         let dates = [];
         for (let i = 0; i < users.length; i++) {
@@ -72,11 +56,9 @@ const Analytics2 = ({ height, color = [] }) => {
     }
 
     function getVirtulData(year) {
-        // console.log(jsonData);
-        let pair = summarizeData(summarize_data);
+        let pair = summarize(summarizeData);
         let keys = pair[0];
         let map = pair[1];
-        // console.log(map);
         year = year || '2022';
         let date = +echarts.number.parseDate(year + '-01-01');
         let end = +echarts.number.parseDate(+year + 1 + '-01-01');
@@ -88,7 +70,6 @@ const Analytics2 = ({ height, color = [] }) => {
             if (idx === -1) {
                 data.push([
                     time_format, 0
-                    // Math.floor(Math.random() * 1000)
                 ]);
             } else {
                 let num = map[idx][1];
@@ -97,101 +78,18 @@ const Analytics2 = ({ height, color = [] }) => {
                     num
                 ]);
             }
-
-            // data.push(map);
         }
         console.log(data);
         return data;
     }
 
-    useEffect(() => {
-        readActivityData("./generated.json")
-    })
-
     const option = {
-        /*
-        grid: {
-            top: '10%',
-            bottom: '10%',
-            left: '5%',
-            right: '5%',
-        },
-        legend: {
-            itemGap: 20,
-            icon: 'circle',
-            textStyle: {
-                color: theme.palette.text.secondary,
-                fontSize: 13,
-                fontFamily: 'roboto',
-            },
-        },
-        tooltip: {},
-        xAxis: {
-            type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-            axisLine: {
-                show: false,
-            },
-            axisTick: {
-                show: false,
-            },
-            axisLabel: {
-                color: theme.palette.text.secondary,
-                fontSize: 14,
-                fontFamily: 'roboto',
-            },
-        },
-        yAxis: {
-            type: 'value',
-            axisLine: {
-                show: false,
-            },
-            axisTick: {
-                show: false,
-            },
-            splitLine: {
-                // show: false
-                lineStyle: {
-                    color: theme.palette.text.secondary,
-                    opacity: 0.15,
-                },
-            },
-            axisLabel: {
-                color: theme.palette.text.secondary,
-                fontSize: 13,
-                fontFamily: 'roboto',
-            },
-        },
-        series: [
-            {
-                data: [30, 40, 20, 50, 40, 80, 90],
-                type: 'line',
-                stack: 'This month',
-                name: 'This month',
-                smooth: true,
-                symbolSize: 4,
-                lineStyle: {
-                    width: 4,
-                },
-            },
-            {
-                data: [20, 50, 15, 50, 30, 70, 95],
-                type: 'line',
-                stack: 'Last month',
-                name: 'Last month',
-                smooth: true,
-                symbolSize: 4,
-                lineStyle: {
-                    width: 4,
-                },
-            },
-        ],*/
         tooltip: {
             position: 'top'
         },
         visualMap: {
             min: 0,
-            max: 1000,
+            max: 10,
             calculable: true,
             orient: 'horizontal',
             left: 'center',
